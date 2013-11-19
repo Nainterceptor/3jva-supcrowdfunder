7862f4628a08dbc7e95fb22ec7ed96c7515fc76b
Gaël Demette <demette.gael@gmail.com>
2013-11-19 18:33:55 +0100

    revert to install compiled bootstrap

---
+++ src/com/supinfo/supcrowdfunder/implementable/User.java
@@ -0,0 +1,10 @@
+package com.supinfo.supcrowdfunder.implementable;
+
+/**
+ * Author: Gaël Demette
+ * Date: 19/11/13
+ * Time: 15:47
+ */
+public interface User {
+    public boolean isEqualPassword(String password);
+}
