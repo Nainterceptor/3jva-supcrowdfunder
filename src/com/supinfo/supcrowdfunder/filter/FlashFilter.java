8d7fa75945145c3424cd97703095aaf51ad28cc9
Gaël Demette <demette.gael@gmail.com>
2013-11-25 14:46:26 +0100

    fixes #244

---
+++ src/com/supinfo/supcrowdfunder/filter/FlashFilter.java
@@ -0,0 +1,34 @@
+package com.supinfo.supcrowdfunder.filter;
+
+import com.supinfo.supcrowdfunder.util.FlashBag;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Author: Gaël Demette
+ * Date: 25/11/13
+ * Time: 00:51
+ */
+@WebFilter(urlPatterns = "/*")
+public class FlashFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpSession session = request.getSession();
+        FlashBag flashbag = new FlashBag(session);
+        req.setAttribute("flashbag", flashbag);
+        chain.doFilter(req, resp);
+        flashbag.persist(request);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
---
+++ src/com/supinfo/supcrowdfunder/util/FlashBag.java
@@ -0,0 +1,52 @@
+package com.supinfo.supcrowdfunder.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author: Gaël Demette
+ * Date: 25/11/13
+ * Time: 00:32
+ */
+public class FlashBag {
+    public Map<String, ArrayList<String>> flashs = new HashMap();
+    private HttpSession session;
+
+    public FlashBag(HttpSession session) {
+        this.session = session;
+        Map<String, ArrayList<String>> flashs = (Map) session.getAttribute("flashbag");
+        if (flashs != null && !flashs.isEmpty())
+            this.flashs = flashs;
+    }
+    public Map<String, ArrayList<String>> getFlashs() {
+        return this.flashs;
+    }
+    public void setFlashs(Map<String, ArrayList<String>> flashs) {
+        this.flashs = flashs;
+    }
+    public ArrayList<String> getFlash(String name) {
+        if (this.flashs.get(name) == null)
+            this.flashs.put(name, new ArrayList<String>());
+        return this.flashs.get(name);
+    }
+    public void addFlash(String name, String label) {
+        ArrayList<String> flashs = this.getFlash(name);
+        flashs.add(label);
+        this.flashs.put(name, flashs);
+    }
+    public ArrayList<String> getOnceFlash(String name) {
+        ArrayList<String> flashs = this.getFlash(name);
+        this.flashs.remove(name);
+        return flashs;
+    }
+    public boolean hasFlashs() {
+        return !this.flashs.isEmpty();
+    }
+    public void persist(HttpServletRequest req) {
+
+        this.session.setAttribute("flashbag", this.flashs);
+    }
+}
