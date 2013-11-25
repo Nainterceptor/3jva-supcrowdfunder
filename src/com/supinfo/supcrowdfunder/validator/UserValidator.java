472f8065c999b6ae54c48c0f079be781014e2c61
Rodrive <robin.lebert@gmail.com>
2013-11-25 16:04:09 +0100

    Ajout de firstname et lastname dans le registration refs #238

--- src/com/supinfo/supcrowdfunder/validator/UserValidator.java
+++ src/com/supinfo/supcrowdfunder/validator/UserValidator.java
@@ -26,4 +26,16 @@ public class UserValidator {
         if (!confirm.equals(password))
             throw new Exception("validator.user.confirmPassword.notEquals");
     }
+    public static void firstname(String firstname) throws Exception {
+        if (firstname == null || firstname.trim().length() == 0)
+            throw new Exception("validator.user.firstname.empty");
+        if (firstname.trim().length() > 255)
+            throw new Exception("validator.user.firstname.tooLong");
+    }
+    public static void lastname(String lastname) throws Exception {
+        if (lastname == null || lastname.trim().length() == 0)
+            throw new Exception("validator.user.lastname.empty");
+        if (lastname.trim().length() > 255)
+            throw new Exception("validator.user.lastname.tooLong");
+    }
 }
