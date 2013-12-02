16e0b2f9124f8cfcc7831188f2e5fbc720fd6670
Gaël Demette <demette.gael@gmail.com>
2013-12-02 23:42:32 +0100

    Requete d'overview

--- src/com/supinfo/supcrowdfunder/dao/StatisticDao.java
+++ src/com/supinfo/supcrowdfunder/dao/StatisticDao.java
@@ -1,6 +1,9 @@
 package com.supinfo.supcrowdfunder.dao;
 
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,5 +27,20 @@ public class StatisticDao extends AbstractDao {
             contributes=0L;
         destroy();
         return contributes;
+    }
+
+    public static Map<String, Long> findGlobalStats() {
+        init();
+        String queryString = "SELECT new map(\n" +
+                "COUNT(u) as nbrUsers, \n" +
+                "(SELECT COUNT(co) FROM Contribute co) as nbrContributes, \n" +
+                "(SELECT COUNT(p) FROM Project p) as nbrProjects, \n" +
+                "(SELECT COUNT(ca) FROM Categorie ca) as nbrCategories, \n" +
+                "(SELECT SUM(co1.amount) FROM Contribute co1) as sumContributes \n" +
+                ") FROM User u";
+        Query query = em.createQuery(queryString, Map.class).setMaxResults(1);
+        Map<String, Long> result = (Map<String, Long>) query.getSingleResult();
+        destroy();
+        return result;
     }
 }
