111d28de9158c7bee0a4ea659ed63b2114475eca
Gaël Demette <demette.gael@gmail.com>
2013-12-03 23:08:54 +0100

    Refactoring Dao WF

--- src/com/supinfo/supcrowdfunder/dao/StatisticDao.java
+++ src/com/supinfo/supcrowdfunder/dao/StatisticDao.java
@@ -1,14 +1,13 @@
 package com.supinfo.supcrowdfunder.dao;
 
 import javax.persistence.Query;
+import java.sql.Timestamp;
 import java.util.Map;
 
 /**
- * Created with IntelliJ IDEA.
  * User: Robin
  * Date: 01/12/13
  * Time: 20:31
- * To change this template use File | Settings | File Templates.
  */
 public class StatisticDao extends AbstractDao {
 
@@ -17,18 +16,15 @@ public class StatisticDao extends AbstractDao {
     }
 
     public static Long sumContributes(Long projectId){
-        init();
-        Query query = em.createQuery("SELECT SUM(c.amount) FROM Contribute c WHERE c.project.id = :projectId", Long.class)
+        Query query = DaoRessource.getEm().createQuery("SELECT SUM(c.amount) FROM Contribute c WHERE c.project.id = :projectId", Long.class)
                 .setParameter("projectId", projectId);
         Long contributes = (Long) query.getSingleResult();
         if (contributes == null)
             contributes=0L;
-        destroy();
         return contributes;
     }
 
     public static Map<String, Long> findGlobalStats() {
-        init();
         String queryString = "SELECT new map(\n" +
                 "COUNT(u) as nbrUsers, \n" +
                 "(SELECT COUNT(co) FROM Contribute co) as nbrContributes, \n" +
@@ -37,9 +33,48 @@ public class StatisticDao extends AbstractDao {
                 "(SELECT SUM(co1.amount) FROM Contribute co1) as sumContributes, \n" +
                 "(SELECT SUM(p1.needCredits) FROM Project p1) as sumNeeded \n" +
                 ") FROM User u";
-        Query query = em.createQuery(queryString, Map.class).setMaxResults(1);
+        Query query = DaoRessource.getEm().createQuery(queryString, Map.class).setMaxResults(1);
         Map<String, Long> result = (Map<String, Long>) query.getSingleResult();
-        destroy();
+        return result;
+    }
+    public static Map<String, Long> findGlobalStatsFrom(Timestamp from) {
+        String queryString = "SELECT new map(\n" +
+                "COUNT(u) as nbrUsers, \n" +
+                "(SELECT COUNT(co) FROM Contribute co) as nbrContributes, \n" +
+                "(SELECT COUNT(p) FROM Project p) as nbrProjects, \n" +
+                "(SELECT COUNT(ca) FROM Categorie ca) as nbrCategories, \n" +
+                "(SELECT SUM(co1.amount) FROM Contribute co1) as sumContributes, \n" +
+                "(SELECT SUM(p1.needCredits) FROM Project p1 WHERE p1.foo >= :from) as sumNeeded \n" +
+                ") FROM User u\n" +
+                " WHERE u.createdAt >= :from";
+        Query query = DaoRessource.getEm().createQuery(queryString, Map.class).setMaxResults(1);
+        Map<String, Long> result = (Map<String, Long>) query.getSingleResult();
+        return result;
+    }
+    public static Map<String, Long> findGlobalStatsTo(Timestamp to) {
+        String queryString = "SELECT new map(\n" +
+                "COUNT(u) as nbrUsers, \n" +
+                "(SELECT COUNT(co) FROM Contribute co) as nbrContributes, \n" +
+                "(SELECT COUNT(p) FROM Project p) as nbrProjects, \n" +
+                "(SELECT COUNT(ca) FROM Categorie ca) as nbrCategories, \n" +
+                "(SELECT SUM(co1.amount) FROM Contribute co1) as sumContributes, \n" +
+                "(SELECT SUM(p1.needCredits) FROM Project p1) as sumNeeded \n" +
+                ") FROM User u\n";
+        Query query = DaoRessource.getEm().createQuery(queryString, Map.class).setMaxResults(1);
+        Map<String, Long> result = (Map<String, Long>) query.getSingleResult();
+        return result;
+    }
+    public static Map<String, Long> findGlobalStatsFromTo(Timestamp from, Timestamp to) {
+        String queryString = "SELECT new map(\n" +
+                "COUNT(u) as nbrUsers, \n" +
+                "(SELECT COUNT(co) FROM Contribute co) as nbrContributes, \n" +
+                "(SELECT COUNT(p) FROM Project p) as nbrProjects, \n" +
+                "(SELECT COUNT(ca) FROM Categorie ca) as nbrCategories, \n" +
+                "(SELECT SUM(co1.amount) FROM Contribute co1) as sumContributes, \n" +
+                "(SELECT SUM(p1.needCredits) FROM Project p1) as sumNeeded \n" +
+                ") FROM User u\n";
+        Query query = DaoRessource.getEm().createQuery(queryString, Map.class).setMaxResults(1);
+        Map<String, Long> result = (Map<String, Long>) query.getSingleResult();
         return result;
     }
 }
